bbb1c3d47c8c7eaa0fbc2ccab9e00bcd925a2432
Andreas Kupries <Andreas Kupries>
2023-02-22 22:23:51 +0000

    rework: changed `astcl` internals to compute pixels by row instead of in a single request. note: this exposed a number of issues, with various operators mishandling such partials. fixes in preceding commits. note: this is also a step towards a general framework usable by future writeres (and supportable as code generation target)

--- op/op.c
+++ op/op.c
@@ -8,11 +8,16 @@
 
 #include <op.h>
 
-//#include <tcl.h>
-//#include <critcl_alloc.h>
-//#include <critcl_assert.h>
-//#include <critcl_trace.h>
-//TRACE_OFF;
+#include <tcl.h>
+#include <critcl_alloc.h>
+#include <critcl_assert.h>
+#include <critcl_trace.h>
+
+// TRACE_ON;
+
+/*
+ * - - -- --- ----- -------- -------------
+ */
 
 #define K(s) Tcl_NewStringObj((s), -1) /* TODO :: Use enum */
 
@@ -22,6 +27,8 @@
 
 static Tcl_Obj*
 aktive_op_astcl (Tcl_Interp* ip, aktive_image src) {
+    TRACE_FUNC ("((interp*) %p, (aktive_image*) %p)", ip, src);
+    
     Tcl_Obj* r = Tcl_NewDictObj();
 
     Tcl_DictObjPut (ip, r, K ("type"),   K (aktive_image_get_type(src)->name));
@@ -37,15 +44,18 @@ aktive_op_astcl (Tcl_Interp* ip, aktive_image src) {
 	Tcl_DictObjPut (ip, r, K("pixels"), pixels);
     }
 
-    return r;
+    TRACE_RETURN ("(Tcl_Obj*) %p", r);
 }
 
 static Tcl_Obj*
 aktive_op_params (Tcl_Interp* ip, aktive_image src) {
-
+    TRACE_FUNC ("((interp*) %p, (aktive_image*) %p)", ip, src);
+    
     aktive_uint c = aktive_image_get_nparams (src);
 
-    if (!c) { return 0; }
+    if (!c) {
+	TRACE_RETURN ("(Tcl_Obj*) %p", 0);
+    }
 	
     Tcl_Obj* p = Tcl_NewDictObj();
 
@@ -55,39 +65,57 @@ aktive_op_params (Tcl_Interp* ip, aktive_image src) {
 	Tcl_DictObjPut (ip, p, K (n), v);
     }
 
-    return p;
+    TRACE_RETURN ("(Tcl_Obj*) %p", p);
 }
 
 static Tcl_Obj*
 aktive_op_pixels (Tcl_Interp* ip, aktive_image src) {
-
+    TRACE_FUNC ("((interp*) %p, (aktive_image*) %p)", ip, src);
+    
     aktive_uint sz = aktive_image_get_size (src);
 
-    if (!sz) { return 0; }
+    if (!sz) {
+	TRACE_RETURN ("(Tcl_Obj*) %p", 0);
+    }
 
     Tcl_Obj* p = Tcl_NewListObj (sz, 0); // 0 => Space is allocated for `sz` elements.
-
-    // This code asks for the entire image in one call.  That is generally bad
-    // for a large image. It also does not allow for concurrent execution.
-    //
-    // %% TODO %% create and use a worker system to for concurrency
     
     aktive_rectangle* domain = aktive_image_get_domain (src);
     aktive_region     rg     = aktive_region_new (src);
-    aktive_block*     pixels = aktive_region_fetch_area (rg, domain);
 
-    for (aktive_uint i = 0; i < sz; i++) {
-	Tcl_Obj* v = Tcl_NewDoubleObj (pixels->pixel [i]);
-	Tcl_ListObjReplace(ip, p, i, 1, 1, &v);
+    // Scan image by rows, add the pixels of each retrieved row to the result list.
+    
+    aktive_rectangle_def_as (scan, domain);
+    aktive_uint height = scan.height;
+    scan.height = 1;
+
+    aktive_uint k, i, j;
+    
+    for (k = 0, i=0; k < height; k ++) {
+	TRACE ("row %d", k);
+	TRACE_RECTANGLE(&scan);
+	
+	aktive_block* pixels = aktive_region_fetch_area (rg, &scan);
+
+	for (j = 0; j < pixels->used; j++, i++) {
+	    ASSERT_VA (i < sz, "too many pixel values", "%d >= %d", i, sz);
+	    
+	    Tcl_Obj* v = Tcl_NewDoubleObj (pixels->pixel [j]);
+	    Tcl_ListObjReplace(ip, p, i, 1, 1, &v);
+	}
+
+	aktive_rectangle_move (&scan, 0, 1);
     }
 
     aktive_region_destroy (rg); // Note that this invalidates `pixels` too.
 
-    return p;
+    TRACE_RETURN ("(Tcl_Obj*) %p", p);
 }
 
 static Tcl_Obj*
 aktive_op_geometry (Tcl_Interp* ip, aktive_image src) {
+    TRACE_FUNC ("((interp*) %p, (aktive_image*) %p)", ip, src);
+    
     Tcl_Obj* geo = Tcl_NewDictObj();
 
     Tcl_DictObjPut (ip, geo, K ("x"),      Tcl_NewIntObj       (aktive_image_get_x      (src)));
@@ -96,7 +124,7 @@ aktive_op_geometry (Tcl_Interp* ip, aktive_image src) {
     Tcl_DictObjPut (ip, geo, K ("height"), aktive_new_uint_obj (aktive_image_get_height (src)));
     Tcl_DictObjPut (ip, geo, K ("depth"),  aktive_new_uint_obj (aktive_image_get_depth  (src)));
 
-    return geo;
+    TRACE_RETURN ("(Tcl_Obj*) %p", geo);
 }
 
 /*
