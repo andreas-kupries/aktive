479e33a1c3ee949c96f5387d97c29a0c68c910a9
Andreas Kupries <Andreas Kupries>
2023-04-12 20:23:46 +0000

    feat: extended `image from matrix` with optional scaling `factor`. chore: updated affected tests.

--- etc/generator/virtual/constant.tcl
+++ etc/generator/virtual/constant.tcl
@@ -95,39 +95,34 @@ operator image::from::matrix {
 
     uint      width   Width of the returned image
     uint      height  Height of the returned image
+    double? 1 factor  Scaling factor
     double... values  Pixel values
 
     state -fields {
 	aktive_block src;
+    } -cleanup {
+	aktive_blit_close (&state->src);
     } -setup {
-	aktive_geometry_set (domain, 0, 0, param->width, param->height, 1);
+	memset (state, 0, sizeof (*state));
+
+	// Create a local copy of the pixel data found in the parameters.
+	// This enables application of scale factor and adding missing values without
+	// affecting the user-visible configuration.
 
 	// Note from the description that the `param.values` is allowed to not contain
-	// enough values for all the pixels. When that is the case we resize the array
-	// to be large enough, and clear the missing parts. This works because:
-	//
-	//  a. The `.values.v` became already heap-allocated when it was copied into the
-	//     image. I.e. it belongs to us now.
-	//
-	//  b. The 0b'00000000 bitpattern represents `(double) 0`.
+	// enough values for all the pixels. In that case the data is extended with zeroes.
 
-	aktive_uint sz = aktive_geometry_get_pixels (domain); // == size, due depth == 1
-	if (param->values.c < sz) {
-	    param->values.v = REALLOC (param->values.v, double, sz);
-	    memset (param->values.v + param->values.c, 0, sz*sizeof (double));
-	}
-
-	// With that done it is now possible to create a pixel storage pointing to the
-	// parameter data, to serve as source when fetching.
-
+	aktive_geometry_set  (domain, 0, 0, param->width, param->height, 1);
 	aktive_geometry_copy (&state->src.domain, domain);
-	state->src.capacity = sz;
-	state->src.used     = sz;
-	state->src.pixel    = param->values.v;
+	aktive_blit_setup    (&state->src, aktive_geometry_as_rectangle (domain));
+	memset (state->src.pixel, 0,               state->src.used * sizeof (double));
+	memcpy (state->src.pixel, param->values.v, param->values.c * sizeof (double));
 
-	// And while the pixel data in the state is technically heap-allocated it does not
-	// belong to the state, wrt responsibility for cleanup. That is handled by the
-	// parameter finalizer. Therefore no need for -cleanup.
+	// Apply the scaling factor, if not identity.
+	if (param->factor != 1) {
+	    aktive_uint i;
+	    for (i = 0; i < param->values.c; i++) { state->src.pixel[i] *= param->factor; }
+	}
     }
     pixels {
 	// Just blit from the source to the destination
