f151588e82ed4c941b3f62bd57729dae664748f5
Andreas Kupries <Andreas Kupries>
2023-11-06 13:24:52 +0000

    feat: global cache of unstructured double arrays note: no users yet

---
+++ runtime/cache.c
@@ -0,0 +1,275 @@
+/* -*- c -*-
+ * - - -- --- ----- -------- -------------
+ *
+ * -- Cache utility functions.
+ *
+ *    Management of a global memory area cache, with LRU eviction policy.
+ *    The cache is oblivious to the data stored in the managed areas.
+ *    Access to the cache from multiple concurrent threads is safe.
+ *
+ * See `cache.h` for the user documentation.
+ */
+
+/*
+ * - - -- --- ----- -------- -------------
+ */
+
+#include <cache.h>
+#include <stddef.h>
+#include <critcl_alloc.h>
+#include <critcl_trace.h>
+#include <critcl_assert.h>
+
+TRACE_OFF;
+
+/*
+ * - - -- --- ----- -------- -------------
+ *
+ * The LRU cache is a circular double-linked list rooted at `cache` (see below).
+ * To ensure circularity the `once()` function properly initializes `cache` as
+ * an empty circle, with all links refering back to the root itself.
+ *
+ * The discard pile is a plain Tcl_HashTable. Each HashEntry's user data is a
+ * circular double-linked list of the discarded/invalidated nodes.
+ */
+
+#define DEFAULT_MAX  (100*1024*1024) // 100 MB
+// #define DEFAULT_MAX (1024*1024*1024) //   1 GB
+
+#define NODE_FROM_AREA(area) \
+    ((aktive_cache_node*) (((char*) (area)) - offsetof (aktive_cache_node, area)))
+
+typedef struct aktive_cache_node *aktive_cache_node_ptr;
+
+typedef struct aktive_cache_node {
+    aktive_cache_node_ptr previous; // LRU chain
+    aktive_cache_node_ptr next;     // LRU chain
+    void*                 owner;
+    aktive_cache_area     area;     // embedded area
+} aktive_cache_node;
+
+/*
+ * - - -- --- ----- -------- -------------
+ */
+
+TCL_DECLARE_MUTEX(cache_lock);
+static aktive_uint       cache_size;
+static aktive_uint       cache_max = DEFAULT_MAX;
+static aktive_cache_node cache = {
+				  0, // LRU previous = bottom/last
+				  0, // LRU next     = top/first
+				  0  //
+};
+
+Tcl_HashTable cache_trim;
+
+static void pretrim  (void);
+static void take     (aktive_cache_node* node);
+static void inittrim (void);
+
+static void once (void) {
+    TRACE_FUNC("", 0);
+    static int once = 0; if (once) { TRACE_RETURN_VOID; } once = 1;
+
+    cache.previous = &cache;
+    cache.next     = &cache;
+    TRACE_RETURN_VOID;
+}
+
+/*
+ * - - -- --- ----- -------- -------------
+ */
+
+extern void
+aktive_cache_set_max (aktive_uint size)
+{
+    once();
+    TRACE_FUNC("(size %u)", size);
+
+    cache_max = size;
+    pretrim();
+
+    TRACE_RETURN_VOID;
+}
+
+extern aktive_uint
+aktive_cache_get_max (void)
+{
+    once();
+    TRACE_FUNC("", 0);
+    TRACE_RETURN("%d", cache_max);
+}
+
+extern aktive_cache_area*
+aktive_cache_new (aktive_uint size, void* owner, void* context)
+{
+    once();
+    TRACE_FUNC("(size %u, owner %p, context %p)", size, owner, context);
+
+    aktive_cache_node* node = ALLOC_PLUS (aktive_cache_node, size+1);
+
+    // Initialize internal parts
+    node->previous = 0;
+    node->next = 0;
+    node->owner = owner;
+
+    // Initialize public parts
+    node->area.context = context;
+    node->area.size = size;
+
+    TRACE_RETURN("(area) %p", &node->area);
+}
+
+extern void
+aktive_cache_release (aktive_cache_area* area)
+{
+    TRACE_FUNC("(area %p)", area);
+
+    aktive_cache_node* node = NODE_FROM_AREA (area);
+    take (node);
+    FREE (node);
+
+    TRACE_RETURN_VOID;
+}
+
+extern void
+aktive_cache_take (aktive_cache_area* area)
+{
+    TRACE_FUNC("(area %p)", area);
+
+    take (NODE_FROM_AREA (area));
+
+    TRACE_RETURN_VOID;
+}
+
+extern void
+aktive_cache_enter (aktive_cache_area* area)
+{
+    TRACE_FUNC("(area %p)", area);
+
+    aktive_cache_node* node = NODE_FROM_AREA (area);
+    Tcl_MutexLock (&cache_lock);
+
+    cache_size += area->size;
+
+    // Insert node at the beginning of the chain
+    node->next           = cache.next;
+    cache.next           = node;
+    node->previous       = &cache;
+    node->next->previous = node;
+
+    pretrim();
+    Tcl_MutexUnlock (&cache_lock);
+
+    TRACE_RETURN_VOID;
+}
+
+extern void
+aktive_cache_trim (void* owner, aktive_cache_trimmer trimmer)
+{
+    TRACE_FUNC("(owner %p, trimmer %p)", owner, trimmer);
+
+    Tcl_MutexLock (&cache_lock);
+    inittrim();
+
+    Tcl_HashEntry* he = Tcl_FindHashEntry (&cache_trim, owner);
+    if (he) {
+	aktive_cache_node *iter, *head = Tcl_GetHashValue (he);
+
+	Tcl_DeleteHashEntry (he);
+
+	while (head->next != head) {
+	    iter = head->next;
+
+	    iter->owner = 0;
+	    iter->previous->next = iter->next;
+	    iter->next->previous = iter->previous;
+
+	    trimmer (owner, &iter->area);
+
+	    FREE (iter);
+	}
+    }
+
+    Tcl_MutexUnlock (&cache_lock);
+
+    TRACE_RETURN_VOID;
+}
+
+/*
+ * - - -- --- ----- -------- -------------
+ */
+
+static void
+take (aktive_cache_node* node)
+{
+    TRACE_FUNC("(node %p)", node);
+
+    Tcl_MutexLock (&cache_lock);
+
+    ASSERT (node->owner, "Do not call release/take from trim.trimmer");
+
+    cache_size -= node->area.size;
+
+    node->previous->next = node->next;
+    node->next->previous = node->previous;
+
+    Tcl_MutexUnlock (&cache_lock);
+
+    node->previous = 0;
+    node->next = 0;
+
+    TRACE_RETURN_VOID;
+}
+
+static void
+inittrim (void)
+{
+    TRACE_FUNC("", 0);
+    static int once = 0; if (once) { TRACE_RETURN_VOID; } once = 1;
+
+    Tcl_InitHashTable (&cache_trim, TCL_ONE_WORD_KEYS);
+    TRACE_RETURN_VOID;
+}
+
+static void
+pretrim (void)
+{
+    TRACE_FUNC("", 0);
+
+    inittrim();
+    // Remove nodes from the end of the chain, i.e. least recently used.
+    while (cache_size > cache_max) {
+	aktive_cache_node* last = cache.previous;
+	take (last);
+
+	// Initialize owner-indexed table of trim lists
+	Tcl_HashEntry* he = Tcl_FindHashEntry (&cache_trim, last->owner);
+	if (!he) {
+	    int new;
+	    Tcl_HashEntry* he = Tcl_CreateHashEntry (&cache_trim, last->owner, &new);
+	    aktive_cache_node* head = ALLOC (aktive_cache_node);
+	    head->previous = head;
+	    head->next     = head;
+	    Tcl_SetHashValue (he, head);
+	}
+
+	// Add removed block into owner's trim list.
+	aktive_cache_node* head = Tcl_GetHashValue (he);
+	last->next = head->next;
+	head->next = last;
+	last->previous = head;
+	last->next->previous = last;
+    }
+
+    TRACE_RETURN_VOID;
+}
+
+/*
+ * = = == === ===== ======== ============= =====================
+ * Local Variables:
+ * mode: c
+ * c-basic-offset: 4
+ * fill-column: 78
+ * End:
+ */
---
+++ runtime/cache.h
@@ -0,0 +1,138 @@
+/* -*- c -*-
+ * - - -- --- ----- -------- -------------
+ *
+ * -- Cache utility functions.
+ *
+ *    Management of a global memory area cache, with LRU eviction policy.
+ *    The cache is oblivious to the data stored in the managed areas.
+ *    Access to the cache from multiple concurrent threads is safe.
+ *
+ * Overview
+ *
+ *    The cache consists of a set of "areas" which are created, released,
+ *    entered into tracking, and taken from tracking.
+ *
+ *    A "memory limit" aka "max size" limits the amount of caching. Going over
+ *    this limit causes the cache to throw out older areas to get under the
+ *    limit again. This is "trimming". The trimmed areas are selected by LRU.
+ *
+ *    Only the "tracked" area count towards the limit.
+ *
+ *    Concurrent access to the cache is allowed, and handled by mutexes,
+ *    i.e. locking the critical sections.  To keep these sections small the
+ *    areas to work are explicitly taken out of and entered into tracking
+ *    (again). While they are not tracked they exclusively belong to the user
+ *    for access.
+ *
+ *    Trimming is done cooperatively with the users in two phases,
+ *
+ *        Phase I, also known as "pre-trimming", happens whenever the cache
+ *        drops old areas out of tracking. These areas are stored in a discard
+ *        pile keyed by the area owners. Area owners are declared when
+ *        creating areas.
+ *
+ *        Phase II happens whenever threads call the "trim" API to complete
+ *        the clean up of the areas for a specific owner. This is cooperative
+ *        because the cache code has no control over when and how often the
+ *        user threads perform this finalization.
+ *
+ *    Trimming was done this way to avoid having one user thread A invoking
+ *    trimming and then not only accessing the global cache structures, but
+ *    also the areas of other user threads B as well.
+ *
+ *   In the chosen design only the pointers to the other thread's areas are
+ *   moved between global cache structures (LRU list to discard pile) and each
+ *   thread is responsible for cleaning up their invalidated areas.
+ *
+ * API
+ *
+ * - set_max, get_max
+ *
+ *   Set / query the memory limit. Setting the size may cause pre-trimming.
+ *   Note: The cache management structures do not count towards the limit.
+ *
+ * - new, release
+ *
+ *   Create / destroy areas. Created areas are not tracked. This happens only
+ *   on their first "enter". Destroying an area takes the area out of tracking
+ *   before the memory is freed.
+ *
+ * - take, enter
+ *
+ *   Takes an area out of tracking, and returns it to tracking.  While the
+ *   area is not tracked the taking thread has exclusive access without
+ *   locking the cache management structures. Only the actions of taking out
+ *   and entering back lock them. Entering an area may cause a pre-trim.
+ *
+ * - trim
+ *
+ *   Complete trimming of invalidated area with a specific owner.
+ *   The `trimmer` function is provided to perform owner-specific actions.
+ *   The `trimmer` function is __not__ permitted to call the "release" or
+ *   "take" API entrypoints.
+ *
+ */
+#ifndef AKTIVE_CACHE_H
+#define AKTIVE_CACHE_H
+
+/*
+ * - - -- --- ----- -------- -------------
+ */
+
+#include <tcl.h>
+#include <base.h>
+
+/*
+ * - - -- --- ----- -------- -------------
+ * - Structure for a cached memory block, with embedded/appended payload.
+ * - Function to call when trimming a cache area is completed.
+ */
+
+typedef struct aktive_cache_area {
+    void*       context; // support information for the area's manager
+    aktive_uint size;    // area size in bytes
+    char        data[0]; // area data, allocated as part of the structure
+} aktive_cache_area;
+
+typedef void (*aktive_cache_trimmer)(void* owner, aktive_cache_area* area);
+
+/*
+ * - - -- --- ----- -------- -------------
+ * - Set maximum cache size. This may cause cache pre-trimming.
+ * - Query the max cache size.
+ * - Generate a new cache block. The new block is __not__ tracked yet.
+ * - Release the cache block. Takes it out of tracking before freeing the memory.
+ * - Take block out of tracking for local access.
+ * - Enter block into tracking. This may cause cache pre-trimming.
+ * - Complete trimming for a specific block owner.
+ *
+ * Cache trimming is done in cooperation with users to reduce the amount and
+ * time of locking needed by the various operations.
+ *
+ * Pre-trimming takes cache blocks out of the main tracking and places them
+ * into a discard table keyed by owners. Owners have to regularly call the
+ * trim API to complete cleaning up their invalidated blocks.
+ *
+ * The trimmer function is responsible for cleaning up and updating the owner
+ * structures for the specific block. It is __not responsible__ for releasing
+ * the block. Calling either of `aktive_cache_release` or `aktive_cache_take`
+ * from the trimmer function is forbidden.
+ */
+
+extern void               aktive_cache_set_max (aktive_uint size);
+extern aktive_uint        aktive_cache_get_max (void);
+extern aktive_cache_area* aktive_cache_new     (aktive_uint size, void* owner, void* context);
+extern void               aktive_cache_release (aktive_cache_area* area);
+extern void               aktive_cache_take    (aktive_cache_area* area);
+extern void               aktive_cache_enter   (aktive_cache_area* area);
+extern void               aktive_cache_trim    (void* owner, aktive_cache_trimmer trimmer);
+
+/*
+ * = = == === ===== ======== ============= =====================
+ * Local Variables:
+ * mode: c
+ * c-basic-offset: 4
+ * fill-column: 78
+ * End:
+ */
+#endif /* AKTIVE_CACHE_H */
