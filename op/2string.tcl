9dc4883672aecbe0559466b0cea25867b669c765
Andreas Kupries <Andreas Kupries>
2024-02-08 20:26:02 +0000

    cleanup: removed unused `aktive::2string` helper note: this removes our dependency on `tcl::chan::variable` as well

--- op/2string.tcl
+++ op/2string.tcl
@@ -2,14 +2,12 @@
 ##
 # AKTIVE -- Andreas Kupries's Tcl Image/Vector Extension
 #
-# (c) 2023 Andreas Kupries http://wiki.tcl.tk/andreas%20kupries
+# (c) 2023-2024 Andreas Kupries http://wiki.tcl.tk/andreas%20kupries
 
 # # ## ### ##### ######## ############# #####################
 
 package require debug           ;# Tcllib
 package require debug::caller   ;# ditto
-
-package require tcl::chan::variable
 
 debug level  aktive/2string
 debug prefix aktive/2string {<[pid]> | }
@@ -17,27 +15,6 @@ debug prefix aktive/2string {<[pid]> | }
 #debug on     aktive/2string
 
 namespace eval aktive {}
-
-# # ## ### ##### ######## ############# #####################
-## Helper to wrap a 2chan image writer so that it returns a string.
-## The helper is not made public.
-
-proc aktive::2string {src args} {
-    debug.aktive/2string {}
-
-    set var ::aktive_[clock microseconds]_[aktive query id $src]
-    set dst [tcl::chan::variable $var]
-
-    uplevel 1 [linsert $args end $src into $dst]
-
-    upvar #0 $var content
-    set r $content
-
-    close $dst
-    unset content
-
-    return $r
-}
 
 # # ## ### ##### ######## ############# #####################
 ## Helper to wrap a 2chan image writer so that it writes to a file
