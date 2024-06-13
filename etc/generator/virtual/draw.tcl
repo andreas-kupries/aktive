d4513d9173ec2342a1a6bbb2a4b15ce99df19f26
Andreas Kupries <Andreas Kupries>
2024-06-13 19:00:29 +0000

    followup: on [5ba079509b] feat: draw multiple circles (varying locations, else identical) fixup: draw circle test oopses

--- etc/generator/virtual/draw.tcl
+++ etc/generator/virtual/draw.tcl
@@ -1,13 +1,69 @@
 ## -*- mode: tcl ; fill-column: 90 -*-
 # # ## ### ##### ######## ############# #####################
-## Generators -- Basic 2D geometrey primitives
+## Generators -- Basic 2D geometry primitives
+#
+## - Circle
+## - Many circles (dots)
+
+operator image::draw::circles {
+    section generator virtual
+
+    note Returns an image with the given dimensions and location
+
+    note Returns an image with the given dimensions and location, showing \
+	a series of (possibly filled) circle with radius and border width, \
+	at the set of centers.
+
+    note Beware, the location and size of the circles are independent of \
+	the image. The operator is perfectly fine with getting a set of \
+	circles completely outside of the image domain.
+
+    note The returned image is single-band, and normally black/white, with the \
+	circle points in white. When aliasing is active however, then partial \
+	border points will be fractionally blended into the background, making \
+	the result gray-scale.
+
+    uint   width   Width of the returned image
+    uint   height  Height of the returned image
+    int? 0 x       Image location, X coordinate
+    int? 0 y       Image location, Y coordinate
+
+    bool? 0 filled Default is an unfilled circle. Filled makes it a disc.
+    bool? 0 alias  If set smooths circle border by blending with the background
+
+    uint? 1 radius Circle radius
+    uint? 0 cwidth Width of the circle's border.
+
+    point... center Circle centers
+
+    body {
+	# This is implemented as a set of circle images aggregated through `max`.
+	set parts [lmap c $center {
+	    aktive image draw circle \
+		x $x y $y width $width height $height \
+		radius $radius cwidth $cwidth center $c \
+		filled $filled alias $alias
+	}]
+	# combine the masks via `max`, binary tree reduction
+	while {[llength $parts] >= 2} {
+	    set odd [expr {[llength $parts] % 2 == 1}]
+	    if {$odd} {
+		set pass  [lindex $parts end]
+		set parts [lrange $parts 0 end-1]
+	    }
+	    set parts [lmap {a b} $parts { aktive op math max $a $b }]
+	    if {$odd} { lappend parts $pass }
+	}
+	# done
+	return [lindex $parts 0]
+    }
+}
 
 operator image::draw::circle {
     section generator virtual
 
-    note Returns an image at the specified location with the given \
-	dimension, where a (possibly filled) circle is drawn, at \
-	given center and radius.
+    note Returns an image with the given dimensions and location, showing \
+	a (possibly filled) circle at the given center, radius, and border width.
 
     note Beware, the location and size of the circle are independent of the image. \
 	The operator is perfectly fine with getting a circle completely outside of \
@@ -23,12 +79,12 @@ operator image::draw::circle {
     int? 0 x       Image location, X coordinate
     int? 0 y       Image location, Y coordinate
 
-    point   center Circle center
-    uint? 1 radius Circle radius
-
     bool? 0 filled Default is an unfilled circle. Filled makes it a disc.
     bool? 0 alias  If set smooths circle border by blending with the background
+
+    uint? 1 radius Circle radius
     uint? 0 cwidth Width of the circle's border.
+    point   center Circle center
 
     state -setup {
 	aktive_geometry_set (domain, param->x, param->y, param->width, param->height, 1);
