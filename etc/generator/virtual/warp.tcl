38932137c918562a911b4794b09d2f390b4b34c4
Andreas Kupries <Andreas Kupries>
2024-11-19 11:35:39 +0000

    feat: additional transform ops. note: inversion, reflections, quadrilaterals, identity. redone: shear, single op, not separate x, y. rework: internals, many low-level code blocks moved into helper procedures. note: mat mul, scale, cofactor, adjoint, invers, determinants 2x2, 3x3, un/boxing. note: higher levels re-assembled from these blocks. rework: transform examples throughout.

--- etc/generator/virtual/warp.tcl
+++ etc/generator/virtual/warp.tcl
@@ -5,8 +5,48 @@
 # https://medium.com/@junfeng142857/
 #                    affine-transformation-why-3d-matrix-for-a-2d-transformation-8922b08bce75
 
+operator transform::invert {
+    section generator virtual warp
+
+    example {
+	aktive transform translate x -5 y -6 | -matrix -label translate x -5 y -6
+	@1                                   | -matrix
+    }
+    example {
+	aktive transform translate x -5 y -6 | -matrix -label translate x -5 y -6
+	aktive transform invert @1           | -matrix -label invert
+	aktive transform compose @1 @2       | -matrix
+    }
+
+    note Takes a single 3x3 projective transformation matrix and returns the \
+	matrix of the inverted transformation. This is used to turn forward \
+	into backward transformations, and vice versa.
+
+    note The result is suitable for use with "<!xref: aktive warp matrix>"
+
+    strict 1st \
+	The projective matrix is materialized and \
+	immediately used to compute the inversion.
+
+    input
+
+    body {
+	# https://www.cuemath.com/algebra/inverse-of-3x3-matrix/
+	return [BOX [Invert [UNBOX input $src]]]
+    }
+}
+
 operator transform::compose {
     section generator virtual warp
+
+    # from right to left: translate, then rotate, then translate back
+    # => rotation around the non-origin point (5,6)
+    example {
+	aktive transform translate x -5 y -6 | -matrix -label translate x -5 y -6
+	aktive transform rotate by 45        | -matrix -label rotate by 45
+	aktive transform translate x 5 y 6   | -matrix -label translate x 5 y 6
+	@1 @2 @3                             | -matrix -label rotate 45 around (5,6)
+    }
 
     note Takes any number of 3x3 projective transformation matrices and returns their \
 	composition.
@@ -14,17 +54,8 @@ operator transform::compose {
     note The result is suitable for use with "<!xref: aktive warp matrix>"
 
     strict all \
-	All projective matrices are materialized \
-	and then used to compute the composition.
-
-    # from right to left: translate, then rotate, then translate back
-    # => rotation around the non-origin point (5,6)
-    example {
-	aktive transform translate dx -5 dy -6 | -matrix
-	aktive transform rotate by 45          | -matrix
-	aktive transform translate dx 5 dy 6   | -matrix
-	@1 @2 @3                               | -matrix
-    }
+	All projective matrices are materialized and \
+	immediately used to compute the composition.
 
     input...
 
@@ -36,150 +67,156 @@ operator transform::compose {
 operator transform::compose-core {
     section generator virtual warp
 
-    note Takes two 3x3 projective transformation matrices and returns their composition.
+    # from right to left: translate, then rotate
+    example {
+	aktive transform rotate by 45      | -matrix -label rotate
+	aktive transform translate x 5 y 6 | -matrix -label translate
+	@1 @2                              | -matrix -label rotate after translate
+    }
+
+    # from right to left: rotate, then translate
+    example {
+	aktive transform translate x 5 y 6 | -matrix -label rotate
+	aktive transform rotate by 45      | -matrix -label translate
+	@1 @2                              | -matrix -label translate after rotate
+    }
+
+    note Takes two 3x3 projective transformation matrices and \
+	returns their composition.
 
     note The result is suitable for use with "<!xref: aktive warp matrix>"
 
     strict both \
 	The two projective matrices are materialized \
-	and then used to compute the composition A*B.
-
-    # from right to left: translate, then rotate
-    example {
-	aktive transform rotate by 45        | -matrix
-	aktive transform translate dx 5 dy 6 | -matrix
-	@1 @2                                | -matrix
-    }
-
-    # from right to left: rotate, then translate
-    example {
-	aktive transform translate dx 5 dy 6 | -matrix
-	aktive transform rotate by 45        | -matrix
-	@1 @2                                | -matrix
-    }
+	and immediately used to compute the composition A*B.
 
     input
     input
 
     body {
-	lassign [aktive query domain $src0] _ _ aw ah
-	if {($aw != 3) || ($ah != 3)} { aktive error "input A is not a projective matrix" }
-	lassign [aktive query domain $src1] _ _ bw bh
-	if {($bw != 3) || ($bh != 3)} { aktive error "input B is not a projective matrix" }
+	BOX \
+	    [Mul \
+		 [UNBOX a $src0] \
+		 [UNBOX b $src1]]
+    }
+}
 
-	set a [aktive query values $src0]
-	set b [aktive query values $src1]
+operator transform::identity {
+    section generator virtual warp
 
-	lassign $a aa ab ac ad ae af ag ah ai
-	lassign $b ba bb bc bd be bf bg bh bi
+    example { | -matrix}
 
-	# | aa ab ac |   | ba bb bc |   | aa*ba+ab*bd+ac*bg aa*bb+ab*be+ac*bh aa*bc+ab*bf+ac*bi |
-	# | ad ae af | * | bd be bf | = | ad*ba+ae*bd+af*bg ad*bb+ae*be+af*bh ad*bc+ae*bf+af*bi |
-	# | ag ah ai |   | bg bh bi |   | ag*ba+ah*bd+ai*bg ag*bb+ah*be+ai*bh ag*bc+ah*bf+ai*bi |
+    note Returns a single-band 3x3 image containing the identity transform.
 
-	# unrolled matrix multiplication
-	set a [expr { $aa*$ba + $ab*$bd + $ac*$bg }]
-	set b [expr { $aa*$bb + $ab*$be + $ac*$bh }]
-	set c [expr { $aa*$bc + $ab*$bf + $ac*$bi }]
-	set d [expr { $ad*$ba + $ae*$bd + $af*$bg }]
-	set e [expr { $ad*$bb + $ae*$be + $af*$bh }]
-	set f [expr { $ad*$bc + $ae*$bf + $af*$bi }]
-	set g [expr { $ag*$ba + $ah*$bd + $ai*$bg }]
-	set h [expr { $ag*$bb + $ah*$be + $ai*$bh }]
-	set i [expr { $ag*$bc + $ah*$bf + $ai*$bi }]
+    note The result is suitable for use with "<!xref: aktive warp matrix>"
 
-	aktive image from matrix width 3 height 3 values $a $b $c $d $e $f $g $h $i
+    # | 1 0 0 |
+    # | 0 1 0 |
+    # | 0 0 1 |
+    body {
+	BOXany \
+	    1 0 0 \
+	    0 1 0 \
+	    0 0 1
     }
 }
 
 operator transform::scale {
     section generator virtual warp
 
+    example {x 3 y 0.5 | -matrix}
+    example {
+	aktive transform scale x 3 y 0.5  | -matrix -label scale x 3 y 1/2
+	aktive transform invert @1        | -matrix -label invert
+    	aktive transform compose @1 @2    | -matrix
+    }
+
     note Returns a single-band 3x3 image specifying a scaling by x- and y factors.
 
     note The result is suitable for use with "<!xref: aktive warp matrix>"
 
-    double? 1 xf Scaling factor for x-axis
-    double? 1 yf Scaling factor for y-axis
+    double? 1 x Scaling factor for x-axis
+    double? 1 y Scaling factor for y-axis
 
-    example {xf 3 yf 0.5 | -matrix}
-
-    # | xf  0 0 |
-    # | 0  yf 0 |
-    # | 0   0 1 |
+    # | x 0 0 |
+    # | 0 y 0 |
+    # | 0 0 1 |
     body {
-	affine \
-	    a $xf b 0 c 0 \
-	    d 0 e $yf f 0
+	BOXany \
+	    $x 0  0 \
+	    0  $y 0 \
+	    0  0  1
     }
 }
 
 operator transform::translate {
     section generator virtual warp
 
+    example {x 3 y 1 | -matrix}
+    example {
+	aktive transform translate x 3 y 1 | -matrix -label translate x 3 y 1
+	aktive transform invert @1           | -matrix -label invert
+	aktive transform compose @1 @2       | -matrix
+    }
+
     note Returns a single-band 3x3 image specifying a translation by x- and y offsets.
 
     note The result is suitable for use with "<!xref: aktive warp matrix>"
 
-    double? 0 dx Translation offset for x-axis
-    double? 0 dy Translation offset for y-axis
+    double? 0 x Translation offset for x-axis
+    double? 0 y Translation offset for y-axis
 
-    example {dx 3 dy 0.5 | -matrix}
-
-    # | 1 0 dx |
-    # | 0 1 dy |
-    # | 0 0  1 |
+    # | 1 0 x |
+    # | 0 1 y |
+    # | 0 0 1 |
     body {
-	affine \
-	    a 1 b 0 c $dx \
-	    d 0 e 1 f $dy
+	BOXany \
+	    1 0 $x \
+	    0 1 $y \
+	    0 0 1
     }
 }
 
-operator transform::shear::x {
+operator transform::shear {
     section generator virtual warp
 
-    note Returns a single-band 3x3 image specifying a shearing along the x-axis.
-
-    note The result is suitable for use with "<!xref: aktive warp matrix>"
-
-    double by	Shear by this many pixels along the x-axis
-
-    example {by 10 | -matrix}
-
-    # | 1 n 0 |
-    # | 0 1 0 |
-    # | 0 0 1 |
-    body {
-	aktive transform affine \
-	    a 1 b $by c 0 \
-	    d 0 e 1   f 0
+    example {x 10    | -matrix}
+    example {y 10    | -matrix}
+    example {x 5 y 3 | -matrix}
+    example {
+	aktive transform shear x 5 y 3 | -matrix -label shear x 5 y 3
+	aktive transform invert @1     | -matrix -label invert
+	aktive transform compose @1 @2 | -matrix
     }
-}
 
-operator transform::shear::y {
-    section generator virtual warp
-
-    note Returns a single-band 3x3 image specifying a shearing along the y-axis.
+    note Returns a single-band 3x3 image specifying a shearing along the axes.
 
     note The result is suitable for use with "<!xref: aktive warp matrix>"
 
-    double by	Shear by this many pixels along the y-axis
+    double? 0 x	Shear by this many pixels along the x-axis
+    double? 0 y	Shear by this many pixels along the y-axis
 
-   example {by 10 | -matrix}
-
-    # | 1 0 0 |
-    # | n 1 0 |
-    # | 0 0 1 |
+    # | XY+1 X 0 |
+    # | Y    1 0 |
+    # | 0    0 1 |
     body {
-	aktive transform affine \
-	    a 1   b 0 c 0 \
-	    d $by e 1 f 0
+	set z [expr {1+$x*$y}]
+	BOXany \
+	    $z $x 0 \
+	    $y 1  0 \
+	    0  0  1
     }
 }
 
 operator transform::rotate {
     section generator virtual warp
+
+    example {by 45 | -matrix}
+    example {
+	aktive transform rotate by 45  | -matrix -label rotate by 45
+	aktive transform invert @1     | -matrix -label invert
+    	aktive transform compose @1 @2 | -matrix
+    }
 
     note Returns a single-band 3x3 image specifying a rotation around the coordinate \
 	origin, by the given angle (in degrees).
@@ -191,31 +228,269 @@ operator transform::rotate {
     # | c -s 0 | where c = cos angle,
     # | s  c 0 | and   s = sin angle
     # | 0  0 1 |
-    example {by 45 | -matrix}
+    body {
+	# note: atan(1)/45 is pi/180, given pi = 4*atan(1)
+	set rad [/ [atan 1] 45]
+	set c   [cos [* $by $rad]]
+	set s   [sin [* $by $rad]]
+	BOXany \
+	    $c [- $s] 0 \
+	    $s $c     0 \
+	    0  0      1
+    }
+}
+
+operator transform::quad::unit {
+    section generator virtual warp
+
+    example { a {1 2} b {6 1} c {7 6} d {2 7} | -matrix }
+    example {
+	aktive transform quad unit a {1 2} b {6 1} c {7 6} d {2 7} | -matrix
+	aktive transform invert @1                                 | -matrix -label invert
+    	aktive transform compose @1 @2                             | -matrix
+    }
+
+    note Returns a single-band 3x3 image transforming the unit square \
+	to the specified quadrilateral.
+
+    note The result is suitable for use with "<!xref: aktive warp matrix>"
+
+    note The quadrilateral is specified as 4 points A-B-C-D in \
+	counter clockwise order. The returned transform maps the \
+	origin of the unit square to A and then the other points in \
+	counter clockwise order.
+
+    note To map between two arbitrary quadrilaterals A and B a composition \
+	of two transforms is necessary and sufficient, i.e. mapping A to \
+	the unit square (as inversion of the map from unit square to A), \
+	followed by mapping the unit square to B. This is what \
+	"<!xref: aktive transform quad quad>" does.
+
+    point a Point A of the quadrilateral
+    point b Point B of the quadrilateral
+    point c Point C of the quadrilateral
+    point d Point D of the quadrilateral
 
     body {
-	set rad [expr {atan(1)/45}] ;# 4*atan(1) = pi, rad = pi/180 -> atan(1)*4/180 -> atan(1)/45
-	set c   [expr {cos($by*$rad)}]
-	set s   [expr {sin($by*$rad)}]
-	set ns  [expr {- $s}]
+	# Calculate the transform from the unit rectangle to the specified quad.
 
-	affine a $c b $ns c 0 d $c e $s f 0
+	# Derived from the paper:
+	#	A Planar Perspective Image Matching using Point Correspondences
+	#	and Rectangle-to-Quadrilateral Mapping
+	# By
+	#	Dong-Keun Kim, Byung-Tae Jang, Chi-Jung Hwang
+	# References
+	#	http://portal.acm.org/citation.cfm?id=884607
+	#	http://www.informatik.uni-trier.de/~ley/db/conf/ssiai/ssiai2002.html
+	#	http://www.decew.net/OSS/References/Quadrilateral%20mapping.pdf
+	#
+	# Errata:
+	# (a) Figure 1 in the paper has p2, p3 (p2', p3') swapped.
+	# (b) The transform matrix A is transposed (or written for left-multiplication).
+
+	# Map from top-left clock-wise for our y-axis.
+	# Flipped relative to regular.
+	#
+	# *---*   *-------* <== Figure 1
+	# |d c|   |p3' p2'|
+	# |   | = |       |
+	# |a b|   |p0' p1'|
+	# *---*   *-------*
+
+	lassign $a ax ay ; # x0, y0
+	lassign $b bx by ; # x1, y1
+	lassign $c cx cy ; # x2, y2
+	lassign $d dx dy ; # x3, y3
+
+	set dxd [expr {$ax - $bx + $cx - $dx}] ; # \delta x3
+	set dyd [expr {$ay - $by + $cy - $dy}] ; # \delta y3
+
+	set dxb [expr {$bx - $cx}]             ; # \delta x1
+	set dxc [expr {$dx - $cx}]             ; # \delta x2
+
+	set dyb [expr {$by - $cy}]             ; # \delta y1
+	set dyc [expr {$dy - $cy}]             ; # \delta y2
+
+	# det | dxb dyb |
+	#     | dxc dyc |
+	set D [expr {($dxb*$dyc - $dyb*$dxc)}]
+
+	set g [expr {($dxd*$dyd - $dxc*$dyd)/double($D)}] ; # a6
+	set h [expr {($dxb*$dyd - $dyb*$dxd)/double($D)}] ; # a7
+
+	# note: a-d writes over the parameters now
+
+	set a [expr {$bx * (1+$g) - $ax}] ; # a0
+	set b [expr {$dx * (1+$h) - $ax}] ; # a1
+	set c $ax                         ; # a2
+
+	set d [expr {$by * (1+$g) - $ay}] ; # a3
+	set e [expr {$dy * (1+$h) - $ay}] ; # a4
+	set f $ay                         ; # a5
+
+	# | a0 a3 a6 |   | a d g | => Errata (b), transpose.
+	# | a1 a4 a7 | = | b e h |
+	# | a2 a5 1  |   | c f 1 |
+	BOXany \
+	    $a $b $c \
+	    $d $e $f \
+	    $g $h 1
+    }
+}
+
+operator transform::quad::quad {
+    section generator virtual warp
+
+    example { a {1 2} b {6 1} c {7 6} d {2 7}   e {0 3} f {7 1} g {8 7} h {1 7} | -matrix }
+    example {
+	aktive transform quad quad a {1 2} b {6 1} c {7 6} d {2 7}   e {0 3} f {7 1} g {8 7} h {1 7} | -matrix
+	aktive transform invert @1     | -matrix -label invert
+    	aktive transform compose @1 @2 | -matrix
+    }
+
+    note Returns a single-band 3x3 image transforming the specified \
+	quadrilateral A to the second quadrilateral B.
+
+    note The result is suitable for use with "<!xref: aktive warp matrix>"
+
+    note The quadrilaterals are specified as 4 points A-B-C-D and \
+	E-F-G-H in counter clockwise order. The returned transform \
+	maps A to E and then the other points in counter clockwise \
+	order.
+
+    point a Point A of the quadrilateral A
+    point b Point B of the quadrilateral A
+    point c Point C of the quadrilateral A
+    point d Point D of the quadrilateral A
+
+    point e Point A of the quadrilateral B
+    point f Point B of the quadrilateral B
+    point g Point C of the quadrilateral B
+    point h Point D of the quadrilateral B
+
+    body {
+	compose [unit a $e b $f c $g d $h] [invert [unit a $a b $b c $c d $d]]
+    }
+}
+operator transform::reflect::x {
+    section generator virtual warp
+
+    example { | -matrix}
+    example {
+	aktive transform reflect x     | -matrix -label reflect x
+	aktive transform invert @1     | -matrix -label invert
+    	aktive transform compose @1 @2 | -matrix
+    }
+
+    note Returns a single-band 3x3 image specifying a reflection along the x-axis.
+
+    note The result is suitable for use with "<!xref: aktive warp matrix>"
+
+    note When not used as part of a chain of transformations then this is \
+	better done using "<!xref: aktive op flip x>"
+
+    body {
+	BOXany \
+	   -1 0 0 \
+	    0 1 0 \
+	    0 0 1
+    }
+}
+
+operator transform::reflect::y {
+    section generator virtual warp
+
+    example { | -matrix}
+    example {
+	aktive transform reflect y        | -matrix -label reflect y
+	aktive transform invert @1        | -matrix -label invert
+    	aktive transform compose @1 @2    | -matrix
+    }
+
+    note Returns a single-band 3x3 image specifying a reflection along the y-axis.
+
+    note The result is suitable for use with "<!xref: aktive warp matrix>"
+
+    note When not used as part of a chain of transformations then this is \
+	better done using "<!xref: aktive op flip y>"
+
+    body {
+	BOXany \
+	    1  0 0 \
+	    0 -1 0 \
+	    0  0 1
+    }
+}
+
+operator transform::reflect::line {
+    section generator virtual warp
+
+    example { a {5 3}           | -matrix}
+    example {
+	aktive transform reflect line a {5 3} | -matrix -label reflect line 0--A
+	aktive transform invert @1            | -matrix -label invert
+    	aktive transform compose @1 @2        | -matrix
+    }
+
+    example { a {5 3} b {-2 -2} | -matrix}
+    example {
+	aktive transform reflect line a {5 3} b {-2 -2} | -matrix -label reflect line A--B
+	aktive transform invert @1                      | -matrix -label invert
+    	aktive transform compose @1 @2                  | -matrix
+    }
+
+    note Returns a single-band 3x3 image specifying a reflection along \
+	either the line through point A and the origin, \
+	or the line through points A and B.
+
+    note The result is suitable for use with "<!xref: aktive warp matrix>"
+
+    point       a Point A of the line to reflect over
+    point? {{}} b Point B of the line to reflect over. If not specified, the origin is used
+
+    body {
+	if {![llength $b]} {
+	    # line through A and origin (0,0).
+	    # this is handled as a chain of three transformations
+	    # (1) rotate around the origin to map the line 0-A on the X-axis.
+	    # (2) reflect along the Y!-axis
+	    # (3) rotate revers around the origin, to get line 0-A back.
+	    #
+	    # note: 45/atan(1) is 180/pi, given pi = 4*atan(1)
+	    lassign $a x y
+	    set angle [expr {atan2($y, $x) * 45 / atan(1)}]
+	    return [compose \
+			[rotate by $angle] \
+			[y] \
+			[rotate by [- $angle]]]
+	}
+
+	# a line through two points A and B is handled by ta chain of three
+	# transformations
+	# (1) translating A to the origin 0.
+	# (2) reflection through the line 0--(B-A)	[recursion]
+	# (3) translating 0 back to A
+
+	lassign $a ax ay
+	lassign $b bx by
+	set ba [list [- $bx $ax] [- $by $ay]]
+
+	return [compose \
+		    [translate x $ax y $ay] \
+		    [line a $ba] \
+		    [translate x [- $ax] y [- $ay]]]
     }
 }
 
 operator transform::affine {
     section generator virtual warp
 
+    example {a 1 b 2 c 3 d 4 e 5 f 6 | -int -matrix}
+
     note Returns a single-band 3x3 image holding the affine \
 	transformation specifed by the 6 parameters a to f.
 
     note The result is suitable for use with "<!xref: aktive warp matrix>"
-
-    # | a b c |
-    # | d e f |
-    # | 0 0 1 |
-
-    example {a 1 b 2 c 3 d 4 e 5 f 6 | -int -matrix}
 
     double a Parameter a of the affine transform
     double b Parameter b of the affine transform
@@ -224,24 +499,26 @@ operator transform::affine {
     double e Parameter e of the affine transform
     double f Parameter f of the affine transform
 
+    # | a b c |
+    # | d e f |
+    # | 0 0 1 |
     body {
-	aktive image from matrix width 3 height 3 values $a $b $c $d $e $f 0 0 1
+	BOXany \
+	    $a $b $c \
+	    $d $e $f \
+	    0  0  1
     }
 }
 
 operator transform::projective {
     section generator virtual warp
 
+    example {a 1 b 2 c 3 d 4 e 5 f 6 g 7 h 8 | -int -matrix}
+
     note Returns a single-band 3x3 image holding the projective \
 	transformation specifed by the 8 parameters a to h.
 
     note The result is suitable for use with "<!xref: aktive warp matrix>"
-
-    # | a b c |
-    # | d e f |
-    # | g h 1 |
-
-    example {a 1 b 2 c 3 d 4 e 5 f 6 g 7 h 8 | -int -matrix}
 
     double a Parameter a of the projective transform
     double b Parameter b of the projective transform
@@ -252,8 +529,14 @@ operator transform::projective {
     double g Parameter g of the projective transform
     double h Parameter h of the projective transform
 
+    # | a b c |
+    # | d e f |
+    # | g h 1 |
     body {
-	aktive image from matrix width 3 height 3 values $a $b $c $d $e $f $g $h 1
+	BOXany \
+	    $a $b $c \
+	    $d $e $f \
+	    $g $h 1
     }
 }
 
---
+++ op/transform.tcl
@@ -0,0 +1,163 @@
+# -*- mode: tcl; fill-column: 90 -*-
+##
+# AKTIVE -- Andreas Kupries's Tcl Image/Vector Extension
+#
+# (c) 2024 Andreas Kupries http://wiki.tcl.tk/andreas%20kupries
+
+# # ## ### ##### ######## ############# #####################
+## Common support for affine and projective transforms.
+## - Basic argument validation and unboxing
+## - Boxing
+## - Determinants of 2x2 and 3x3 matrices
+## - Mat mul, inversion, cofactors, adjoint
+
+namespace eval ::aktive::transform {
+    # for simple expressions direct access to ops and funcs
+    namespace import ::tcl::mathfunc::*
+    namespace import ::tcl::mathop::*
+}
+
+proc aktive::transform::PRINT {label m} {
+    lassign $m a b c d e f g h i
+    join [list ----$label "$a $b $c" "$d $e $f" "$g $h $i"] \n
+}
+
+proc aktive::transform::BOXany {args} {
+    BOX $args
+}
+
+proc aktive::transform::BOX {m} {
+    aktive image from matrix width 3 height 3 values {*}$m
+}
+
+proc aktive::transform::UNBOX {label src} {
+    lassign [aktive query domain $src] _ _ w h
+    if {($w == 3) && ($h == 3)} {
+	return [aktive query values $src]
+    }
+    aktive error "$label is not a projective transform"
+    return
+}
+
+proc aktive::transform::Mul {a b} {
+    lassign $a aa ab ac ad ae af ag ah ai
+    lassign $b ba bb bc bd be bf bg bh bi
+
+    # | aa ab ac |   | ba bb bc |   | aa*ba+ab*bd+ac*bg aa*bb+ab*be+ac*bh aa*bc+ab*bf+ac*bi |
+    # | ad ae af | * | bd be bf | = | ad*ba+ae*bd+af*bg ad*bb+ae*be+af*bh ad*bc+ae*bf+af*bi |
+    # | ag ah ai |   | bg bh bi |   | ag*ba+ah*bd+ai*bg ag*bb+ah*be+ai*bh ag*bc+ah*bf+ai*bi |
+
+    # unrolled matrix multiplication
+    set a [expr { $aa*$ba + $ab*$bd + $ac*$bg }]
+    set b [expr { $aa*$bb + $ab*$be + $ac*$bh }]
+    set c [expr { $aa*$bc + $ab*$bf + $ac*$bi }]
+    set d [expr { $ad*$ba + $ae*$bd + $af*$bg }]
+    set e [expr { $ad*$bb + $ae*$be + $af*$bh }]
+    set f [expr { $ad*$bc + $ae*$bf + $af*$bi }]
+    set g [expr { $ag*$ba + $ah*$bd + $ai*$bg }]
+    set h [expr { $ag*$bb + $ah*$be + $ai*$bh }]
+    set i [expr { $ag*$bc + $ah*$bf + $ai*$bi }]
+
+    list $a $b $c $d $e $f $g $h $i
+}
+
+proc aktive::transform::Invert {m} {
+    set inv [ScaleDown [Adjoint $m] [DET3 $m]]
+    puts [PRINT inv $inv]
+    set inv
+}
+
+proc aktive::transform::ScaleDown {m v} {
+    lassign $m a b c d e f g h i
+    set a [/ $a $v]
+    set b [/ $b $v]
+    set c [/ $c $v]
+    set d [/ $d $v]
+    set e [/ $e $v]
+    set f [/ $f $v]
+    set g [/ $g $v]
+    set h [/ $h $v]
+    set i [/ $i $v]
+    list $a $b $c $d $e $f $g $h $i
+}
+
+proc aktive::transform::Adjoint {m} {
+    # https://www.cuemath.com/algebra/inverse-of-3x3-matrix/
+    # adjoint is the transposed cofactor
+    # | a d g |
+    # | b e h |
+    # | c f i |
+    lassign [Cofactors $m] a b c   d e f   g h i
+    # transpose
+    list $a $d $g   $b $e $h   $c $f $i
+}
+
+proc aktive::transform::Cofactors {m} {
+    # https://www.cuemath.com/algebra/cofactor-matrix/
+    # https://www.cuemath.com/algebra/inverse-of-3x3-matrix/
+    lassign $m a b c d e f g h i
+    # | a b c |
+    # | d e f |
+    # | g h i |
+    set ca [DET2 $e $f $h $i]	;# cofactor matrix
+    set cb [DET2 $d $f $g $i]	;# | +ca -cb +cc | 0
+    set cc [DET2 $d $e $g $h]	;# | -cd +ce -cf | 1
+    set cd [DET2 $b $c $h $i]	;# | +cg -ch +ci | 2
+    set ce [DET2 $a $c $g $i]	;#   0   1   2
+    set cf [DET2 $a $b $g $h]	;#
+    set cg [DET2 $b $c $e $f]	;# note the alternating signs
+    set ch [DET2 $a $c $d $f]	;# sign = (-1)^(i+j)
+    set ci [DET2 $a $b $d $e]
+
+    list $ca [- $cb] $cc   [- $cd] $ce [- $cf]   $cg [- $ch] $ci
+}
+
+proc aktive::transform::DET2 {a b c d} {
+    # determinant of the 2x2 matrix
+    ##
+    # det | a b | = ad-bc / difference of the
+    #     | c d |           products of the
+    #                       diagonals
+    expr {$a * $d - $b * $c}
+}
+
+proc aktive::transform::DET3 {m} {
+    # determinant of the 3x3 matrix
+    # https://www.cuemath.com/algebra/inverse-of-3x3-matrix/
+    #
+    # | a b c | => | a b c a b c | aei + bfg + cdh - afh - bdi - ceg
+    # | d e f |	   |  \ \ x / /  |
+    # | g h i |	   | d e f d e f |
+    #		   |    x x x    |
+    #		   | g h i g h i |
+    #
+    lassign $m a b c d e f g h i
+    expr {$a*$e*$i + $b*$f*$g + $c*$d*$h - $a*$f*$h - $b*$d*$i - $c*$e*$g}
+}
+
+# # ## ### ##### ######## ############# #####################
+## enable the deeper nested transform commands (reflect, quad) to use the base commands
+
+namespace eval ::aktive::transform::quad {
+    namespace import ::aktive::transform::compose
+    namespace import ::aktive::transform::invert
+    # for simple expressions direct access to ops and funcs
+    namespace import ::tcl::mathfunc::*
+    namespace import ::tcl::mathop::*
+}
+
+namespace eval ::aktive::transform::reflect {
+    namespace import ::aktive::transform::compose
+    namespace import ::aktive::transform::rotate
+    namespace import ::aktive::transform::translate
+    # for simple expressions direct access to ops and funcs
+    namespace import ::tcl::mathfunc::*
+    namespace import ::tcl::mathop::*
+}
+
+# import non-exported internal commands.
+interp alias {} ::aktive::transform::quad::BOXany    {} ::aktive::transform::BOXany
+interp alias {} ::aktive::transform::reflect::BOXany {} ::aktive::transform::BOXany
+
+# # ## ### ##### ######## ############# #####################
+return
