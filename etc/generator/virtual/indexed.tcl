644b4f9eb25d899f61f8314efba1875905517cd3
Andreas Kupries <Andreas Kupries>
2024-11-20 14:39:44 +0000

    feat: support non-origin x/y location for indexed. fix:  un/signed mismatch causing bogus index values for negative locations.

--- etc/generator/virtual/indexed.tcl
+++ etc/generator/virtual/indexed.tcl
@@ -5,19 +5,22 @@
 operator image::indexed {
     section generator virtual
 
-    note Returns a 2-band image where each pixel declares its own position
+    example {          width 5 height 5 | -int -matrix}
+    example {x -5 y -5 width 8 height 8 | -int -matrix}
+
+    note Returns a 2-band image where each pixel declares its own position.
 
     note __Note__ that while the result is usable as a warp map for \
 	the "<!xref aktive op warp map>" operation, it will not do \
 	anything, as it represents the identity warping.
 
-    example {width 5 height 5 | -int -matrix}
-
-    uint   width   Width of the returned image
-    uint   height  Height of the returned image
+    uint   width  Width of the returned image
+    uint   height Height of the returned image
+    int? 0 x      X location of the returned image in the 2D plane
+    int? 0 y      Y location of the returned image in the 2D plane
 
     state -setup {
-	aktive_geometry_set (domain, 0, 0, param->width, param->height, 2);
+	aktive_geometry_set (domain, param->x, param->y, param->width, param->height, 2);
     }
 
     blit indexed {
@@ -25,7 +28,7 @@ operator image::indexed {
 	{AW {x AX 1 up} {x SX 1 up}}
 	{DD {z  0 1 up} {z  0 1 up}}
     } {point {
-	(z == 0) ? x : y
+	(z == 0) ? ((int) x) : ((int) y)
     }}
 
     pixels {
