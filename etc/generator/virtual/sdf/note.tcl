edbf190459078ff27b47b3801f7df0aebc099ed3
Andreas Kupries <Andreas Kupries>
2024-11-22 13:01:52 +0000

    fix: broken location pass-through when drawing. tweak: documentation.

--- etc/generator/virtual/sdf/note.tcl
+++ etc/generator/virtual/sdf/note.tcl
@@ -1,4 +1,4 @@
 
-note Beware, the location and size of the $element are independent of the image dimensions. \
-    The operator is perfectly fine computing the SDF of a $element located completely \
-    outside of the image domain.
+note Beware, the location and size of the $element are independent of image location and \
+    dimensions. The operator is perfectly fine computing the SDF of a $element located \
+    completely outside of the image domain.
