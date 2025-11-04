5c950356a5d289edf9a53f244c037bee9b2d7803
Andreas Kupries <Andreas Kupries>
2025-11-04 21:02:34 +0000

    fixup: undone unwanted changes committed to build(-only) and trial.tcl

--- tools/trial.tcl
+++ tools/trial.tcl
@@ -10,42 +10,5 @@ source [file join $top tools trial-base.tcl]
 
 # ------------------------------------------------------------------------------
 
-set base [aktive op select x [gradx] from 0 to 4]
-
-puts [pixels aktive op align left $base border black size 7]
-#show $out
-
-exit
-
-set  in [aktive read from netpbm file path [A crop.pgm]]
-set  in [aktive op equalization global $in]
-show $in
-
-exit
-
-# ------------------------------------------------------------------------------
-
-set in  [gradx]
-set ni  [aktive op flip x $in]
-
-set sub [aktive op math sub $in $ni]
-set fin [aktive op select x $sub from 0 to 18]
-
-set sin [aktive op select x $in from 0 to 18]
-set sni [aktive op select x $ni from 0 to 18]
-set ssb [aktive op math sub $sin $sni]
-
-puts =============================================
-#show $in
-#show $ni
-#show $sub
-show $fin
-exit
-puts =============================================
-show $sin
-show $sni
-show $ssb
-puts =============================================
-
 # ------------------------------------------------------------------------------
 exit
